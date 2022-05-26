236a6fae82c2e76fa64348d90417f1cfe41280dc
Mary abbasi <abbasi67m@gmail.com>
2022-05-26 15:22:03 +0300

    Initial Commit

---
+++ projectgit/projectgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectgitApp.swift
+//  projectgit
+//
+//  Created by Maryam Abbasi on 26.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct projectgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
